41eb7c113d35b0fe2d635fa4147f6e7bbb9d4d14
mdfirdause7224 <mdfirdause7224@gmail.com>
2025-04-09 18:21:28 +0530

    Buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,36 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Button</Button>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div className="flex gap-2 mt-10">
+        <div>
+          <div className="mb-4">
+            <Button variant="default">Default</Button>
+          </div>
+          <div className="mb-4">
+            <Button variant="primary">Primary</Button>
+          </div>
+          <div className="mb-4">
+            <Button variant="success">Success</Button>
+          </div>
+          <div className="mb-4">
+            <Button variant="link">Link</Button>
+          </div>
+        </div>
+        <div>
+          <div className="mb-4">
+            <Button variant="info">Info</Button>
+          </div>
+          <div className="mb-4">
+            <Button variant="warning">Warning</Button>
+          </div>
+          <div className="mb-4">
+            <Button variant="danger">Danger</Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,19 +9,16 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: "text-white bg-transparent border border-zinc-700",
+        primary: "bg-emerald-500 text-white hover:bg-emerald-600",
+        success: "bg-blue-500 text-white hover:bg-blue-600",
+        info: "bg-cyan-500 text-white hover:bg-cyan-600",
+        warning: "bg-orange-400 text-white hover:bg-orange-500",
+        danger: "bg-rose-500 text-white hover:bg-rose-600",
+        link: "text-emerald-500",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "w-36 h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
