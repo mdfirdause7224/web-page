4fc4dce7429add92fba6d6b0391f7d64888b967f
mdfirdause7224 <mdfirdause7224@gmail.com>
2025-04-10 10:51:39 +0530

    success

--- src/App.tsx
+++ src/App.tsx
@@ -9,34 +9,58 @@ function App() {
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
       <div className="grid grid-cols-3 gap-4 mt-10 p-4">
-        <Panel variant="default" title="Default Panel">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.
-        </Panel>
-        <Panel variant="primary" title="Primary Panel">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.
-        </Panel>
-        <Panel variant="success" title="Success Panel">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.
-        </Panel>
-        <Panel 
-          variant="info" 
-          title="Info Panel"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.
-        </Panel>
-        <Panel 
-          variant="warning" 
-          title="Warning Panel"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.
-        </Panel>
-        <Panel 
-          variant="danger" 
-          title="Danger Panel with Footer"
-          footer="Panel Footer"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.
-        </Panel>
+        <div className="">
+          <div className="mb-3">
+            <Panel variant="default" title="Default Panel">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare
+              lacus adipiscing, posuere lectus et, fringilla augue.
+            </Panel>
+          </div>
+          <div>
+            <Panel variant="primary" title="Primary Panel">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare
+              lacus adipiscing, posuere lectus et, fringilla augue.
+            </Panel>
+          </div>
+        </div>
+        <div className="">
+          <div className="mb-3 ">
+            <Panel variant="success" title="Success Panel">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare
+              lacus adipiscing, posuere lectus et, fringilla augue.
+            </Panel>
+          </div>
+          <div>
+            <Panel variant="info" title="Info Panel">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare
+              lacus adipiscing, posuere lectus et, fringilla augue.
+            </Panel>
+          </div>
+        </div>
+        <div className="">
+          <div className="mb-3">
+            <Panel variant="warning" title="Warning Panel">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare
+              lacus adipiscing, posuere lectus et, fringilla augue.
+            </Panel>
+          </div>
+          <div>
+            <Panel
+              variant="danger"
+              title="Danger Panel with Footer"
+              footer="Panel Footer"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare
+              lacus adipiscing, posuere lectus et, fringilla augue.
+            </Panel>
+          </div>
+        </div>
       </div>
 
       <div className="flex gap-2 mt-10">
